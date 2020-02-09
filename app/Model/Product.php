1a0de77241d1ccc9768a2e6f3f9cde3526c90782
Saphal <gsaphal99@gmail.com>
2020-02-09 20:39:42 +0545

    Product Update

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -5,8 +5,13 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
  class Product extends Model
 {
+    protected $fillable=[
+        'name','detail','price','stock','discount'
+
+    ];
    public function reviews()
    {
+    
        return $this->hasMany(Review::class);
    }
 }
