6d5a03de1b77d75d4ccad34e531583f18548a7a7
Saphal <gsaphal99@gmail.com>
2020-02-09 22:20:09 +0545

    Created Update and Delete on Review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,7 +6,6 @@ use App\Model\Review;
 use Illuminate\Http\Request;
 use App\Model\Product;
 use Symfony\Component\HttpFoundation\Response;
-
 use App\Http\Resources\ReviewResource;
 use App\Http\Requests\ReviewRequest;
 class ReviewController extends Controller
@@ -75,9 +74,13 @@ class ReviewController extends Controller
      * @param  \App\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Review $review)
+    public function update(Request $request, Product $product,Review $review)
     {
-        //
+       $review->update($request->all());
+       return response([
+        'data'=>new ReviewResource($review)
+    ],Response::HTTP_CREATED);
+
     }
 
     /**
@@ -86,8 +89,9 @@ class ReviewController extends Controller
      * @param  \App\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product,Review $review)
     {
-        //
+       $review->delete();
+       return response(null,Response::HTTP_NO_CONTENT);
     }
 }
